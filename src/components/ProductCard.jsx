335ae50d6b307d89f70e92286478dc99e0aa4ac5
EC <casianoemmanuel2217@gmail.com>
2026-02-07 16:31:36 +0800

    Fixed import path link

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router";
+import { Link } from "react-router-dom";
 export default function ProductCard({ product }) {
     return (
         <div className="product-card">
