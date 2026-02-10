3ed22c6e2754be1a811b3b47522643f8e68fcac6
EC <casianoemmanuel2217@gmail.com>
2026-02-11 02:48:37 +0800

    Finished cart and checkout

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,5 +1,10 @@
 import { Link } from "react-router-dom";
+import { useCart } from "../context/CartContext";
 export default function ProductCard({ product }) {
+    const { addToCart, cartItems } = useCart();
+    const productInCart = cartItems.find((item) => item.id === product.id);
+
+    const productQuantityLabel = productInCart ? `(${productInCart.quantity})` : "";
     return (
         <div className="product-card">
             <img src={product.image} className="product-card-image" alt={product.name} />
@@ -10,7 +15,9 @@ export default function ProductCard({ product }) {
                     <Link className="btn btn-secondary" to={`/products/${product.id}`}>
                         View Details
                     </Link>
-                    <button className="btn btn-primary">Add to Cart</button>
+                    <button className="btn btn-primary" onClick={() => addToCart(product.id)}>
+                        Add to Cart {productQuantityLabel}
+                    </button>
                 </div>
             </div>
         </div>
